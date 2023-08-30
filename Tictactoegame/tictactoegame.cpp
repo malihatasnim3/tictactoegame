54e4d419b7ff634c2ebb0aa6b28f88f2aa0b582f
Maliha Tasnim <malihatasnim753@gmail.com>
2023-08-30 23:25:29 +0800

    display function for the board

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+using namespace std;
+
+void display(char board[3][3]) {
+		for (int i = 0; i < 2; i++) {
+			cout << " " << board[i][0] << " | " << board[i][1] << " | " << board[i][2] << endl;
+			cout << "-----------" << endl;
+		}
+		cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+}
+
+int main() {
+	char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
+	display(board);
+}
