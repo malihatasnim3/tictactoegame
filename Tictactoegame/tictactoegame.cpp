3afa2a5794b4efa3e728c043257ff3c918a887ef
Maliha Tasnim <malihatasnim753@gmail.com>
2023-08-31 00:10:29 +0800

    input validation inside the main function

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -20,17 +20,47 @@ void insertInput(char i, char board[3][3], void display(char board[3][3])) {
 	case 2 : board[0][1] = i ;
 		display(board);
 		break;
+	case 3: board[0][2] = i;
+		display(board);
+		break;
+	case 4: board[1][0] = i;
+		display(board);
+		break;
+	case 5: board[1][1] = i;
+		display(board);
+		break;
+	case 6: board[1][2] = i;
+		display(board);
+		break;
+	case 7: board[2][0] = i;
+		display(board);
+		break;
+	case 8: board[2][1] = i;
+		display(board);
+		break;
+	case 9: board[2][2] = i;
+		display(board);
+		break;
 	default:
 		cout << "Please enter a valid position" << endl;
 		break;
-
 	}
 }
 
 int main() {
 	char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
 	char input;
+	display(board);
 	cout << "Player 1: Enter input: " << endl;
 	cin >> input;
-	insertInput(input, board, display);
+	while (true) {
+		if (input == 'x' || input == 'X' || input == 'o' || input == 'O') {
+			insertInput(input, board, display);
+		}
+		else {
+			cout << "Please enter a valid input: " << endl;
+			cin >> input;
+		}
+	}
+
 }
