bd914f4e1e404b6c182adf35d7a2ddcb3aa092af
Maliha Tasnim <malihatasnim753@gmail.com>
2023-08-31 00:01:58 +0800

    taking the first input from the user

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -9,7 +9,28 @@ void display(char board[3][3]) {
 		cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
 }
 
+void insertInput(char i, char board[3][3], void display(char board[3][3])) {
+	int position;
+	cout << "Choose a position from 1-9: " << endl;
+	cin >> position;
+	switch (position) {
+	case 1 : board[0][0] = i ;
+		display(board);
+		break;
+	case 2 : board[0][1] = i ;
+		display(board);
+		break;
+	default:
+		cout << "Please enter a valid position" << endl;
+		break;
+
+	}
+}
+
 int main() {
 	char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
-	display(board);
+	char input;
+	cout << "Player 1: Enter input: " << endl;
+	cin >> input;
+	insertInput(input, board, display);
 }
