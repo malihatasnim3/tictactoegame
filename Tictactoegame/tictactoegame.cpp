0c129983a782447a7044ce25cd0d332d1a49d659
Maliha Tasnim <malihatasnim753@gmail.com>
2023-08-31 21:37:42 +0800

    modified insertInput function with do while loop

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void display(char board[3][3]) {
@@ -10,42 +11,68 @@ void display(char board[3][3]) {
 }
 
 void insertInput(int p, char i, char board[3][3], void display(char board[3][3])) {
-	cout << "Choose a position from 1-9: " << endl;
-	cin >> p;
-	switch (p) {
-	case 1 : board[0][0] = i ;
-		display(board);
-		break;
-	case 2 : board[0][1] = i ;
-		display(board);
-		break;
-	case 3: board[0][2] = i;
-		display(board);
-		break;
-	case 4: board[1][0] = i;
-		display(board);
-		break;
-	case 5: board[1][1] = i;
-		display(board);
-		break;
-	case 6: board[1][2] = i;
-		display(board);
-		break;
-	case 7: board[2][0] = i;
-		display(board);
-		break;
-	case 8: board[2][1] = i;
-		display(board);
-		break;
-	case 9: board[2][2] = i;
-		display(board);
-		break;
-	default:
-		cout << "Please enter a valid position" << endl;
-		break;
-	}
+
+	 do{
+		 cout << "Choose a position from 1-9: " << endl;
+		 cin >> p;
+		{
+			if (p == 1) {
+				board[0][0] = i;
+				display(board);
+				break;
+			}
+			else if (p == 2) {
+				board[0][1] = i;
+				display(board);
+			}
+			else if (p == 3) {
+				board[0][2] = i;
+				display(board);
+			}
+			else if (p == 4) {
+				board[1][0] = i;
+				display(board);
+			}
+			else if (p == 5) {
+				board[1][1] = i;
+				display(board);
+			}
+			else if (p == 6) {
+				board[1][2] = i;
+				display(board);
+			}
+			else if (p == 7) {
+				board[2][0] = i;
+				display(board);
+			}
+			else if (p == 8) {
+				board[2][1] = i;
+				display(board);
+			}
+			else if (p == 9) {
+				board[2][2] = i;
+				display(board);
+			}
+			else {
+				cout << "Invalid input." << endl;
+			}
+		}
+	 } while (!(p != 0 && p <= 9));
 }
 
+
+
+int validatePosition(int p){
+	vector<int> enteredP;
+	while (true) {
+		if (find(enteredP.begin(), enteredP.end(), p) != enteredP.end()) {
+			cout << "Position already entered. Please enter a different position: ";
+		}
+		else {
+			enteredP.push_back(p);
+		}
+	}
+}
 
 int main() {
 	char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
@@ -67,32 +94,18 @@ int main() {
 	}
 
 
-	//PLAYER 2
-	cout << "Player 2: Enter input: " << endl;
-	cin >> input2;
-	while (true) {
-		if (input1 == input2) {
-			cout << "Please enter a valid input: " << endl; \
-				cin >> input2;
-		}
-		else {
-			if (input2 == 'x' || input2 == 'o') {
-				if (position1 != position2) {
-					insertInput(position2, input2, board, display);
-				}
-				else {
-					cout << "Please enter a valid position: " << endl;
-					cin >> position2;
-				}
-			}
-			else {
-				cout << "Please enter a valid input: " << endl;
-				cin >> input2 ;
-			}
-	
-
-		}
-	}
+	////PLAYER 2
+	//cout << "Player 2: Enter input: " << endl;
+	//cin >> input2;
+	//while (true) {
+	//	if (input1 == input2) {
+	//		cout << "Please enter a valid input: " << endl; \
+	//			cin >> input2;
+	//	}
+	//	else {
+	//		
+	//	}
+	//}
 
 
 }
