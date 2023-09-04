14fffe33a3537184ee340a288d8690f3fb118df0
Maliha Tasnim <malihatasnim753@gmail.com>
2023-09-04 19:56:00 +0800

    input taking from two players

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
-char input = ' ';
 int position = 0;
+char playerTurn = ' ';
+bool gameOver = false;
 
 void display() {
 		for (int i = 0; i < 2; i++) {
@@ -14,54 +15,57 @@ void display() {
 		cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
 }
 
-void insertPosition(void display(char board[3][3])) {
-	if (input == 'X') {
-		cout << "Player [X] enter position: " << endl;
-	}
-	else {
-		cout << "Player[O] enter position:" << endl;
-	}
+void insertPosition(void (display)()) {
 
 	 do{
 		 cout << "Choose a position from 1-9: " << endl;
 		 cin >> position;
-		{
-			if (position == 1) {
-				board[0][0] = input;
-				display(board);
+		 {
+			if (position == 1 && board[0][0] != 'X' && board[0][0] != 'O') {
+				board[0][0] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
 				break;
 			}
-			else if (position == 2) {
-				board[0][1] = input;
-				display(board);
+			else if (position == 2 && board[0][1] != 'X' && board[0][1] != 'O') {
+				board[0][1] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 3) {
-				board[0][2] = input;
-				display(board);
+			else if (position == 3 && board[0][2] != 'X' && board[0][2] != 'O') {
+				board[0][2] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
+;
 			}
-			else if (position == 4) {
-				board[1][0] = input;
-				display(board);
+			else if (position == 4 && board[1][0] != 'X' && board[1][0] != 'O') {
+				board[1][0] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 5) {
-				board[1][1] = input;
-				display(board);
+			else if (position == 5 && board[1][1] != 'X' && board[1][1] != 'O') {
+				board[1][1] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 6) {
-				board[1][2] = input;
-				display(board);
+			else if (position == 6 && board[1][2] != 'X' && board[1][2] != 'O') {
+				board[1][2] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 7) {
-				board[2][0] = input;
-				display(board);
+			else if (position == 7 && board[2][0] != 'X' && board[2][0] != 'O') {
+				board[2][0] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 8) {
-				board[2][1] = input;
-				display(board);
+			else if (position == 8 && board[2][1] != 'X' && board[2][1] != 'O') {
+				board[2][1] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
-			else if (position == 9) {
-				board[2][2] = input;
-				display(board);
+			else if (position == 9 && board[2][2] != 'X' && board[2][2] != 'O') {
+				board[2][2] = playerTurn;
+				playerTurn = (playerTurn == 'X') ? 'O' : 'X';
+				break;
 			}
 			else {
 				cout << "Invalid input." << endl;
@@ -70,17 +74,26 @@ void insertPosition(void display(char board[3][3])) {
 	 } while (!(position != 0 && position <= 9));
 }
 
-void insertInput(char i, char board[3][3], void display(char board[3][3]), void insertPosition()) {
-	cout << "Enter input: " << endl;
-	cin >> i;
-
+void currentPlayer() {
+	cout << "Enter your input (X/O): " << endl;
+	cin >> playerTurn;
+	if (playerTurn == 'X') {
+			cout << "Player [X] turn : ";
+	}
+	else if (playerTurn == 'O') {
+			cout << "Player [O] turn : ";
+	}
 }
 
 
 
 int main() {
 	
-	display();
+	while (!gameOver) {
+		display();
+		currentPlayer();
+		insertPosition(display);
+	}
 	
 
 }
