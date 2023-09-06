3aae5f2f6c2ace742cb1a63786b2879e804bc30c
Maliha Tasnim <malihatasnim753@gmail.com>
2023-09-06 18:58:25 +0800

    executing game over function

--- Tictactoegame/tictactoegame.cpp
+++ Tictactoegame/tictactoegame.cpp
@@ -5,7 +5,7 @@ using namespace std;
 char board[3][3] = { {' ',' ', ' '}, {' ', ' ', ' '} , {' ', ' ', ' '} };
 int position = 0;
 char playerTurn = 'X';
-bool gameOver = false;
+bool draw = true;
 
 void display() {
 		for (int i = 0; i < 2; i++) {
@@ -84,13 +84,71 @@ void currentPlayer() {
 }
 
 
+bool gameWinner() {
+	for (int i = 0; i < 3; i++) {
+		if ((board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+			(board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])) {
+			return false;
+		}
+
+		else if ((board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) || 
+			(board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+			return false;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] != 'X' && board[i][j] != 'O') {
+				return true;
+			}
+		}
+	}
+	draw == true;
+	return false;
+}
+
+
+
+	/*}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] == ' ') {
+				draw = false;
+				return draw;
+			}
+			else if (board[i][j] != ' ' && (board[i][j] == 'X' || board[i][j] == 'O')) {
+				draw = true;
+				return draw;
+			}
+		}
+	}
+	return false;*/
+
+
+
+	//for (int i = 0; i < 3; i++) {
+	//	for (int j = 0; j < 3; j++) {
+	//		if (board[i][j] != 'X' && board[i][j] != 'O')
+	//			return false;
+	//	}
+	//}
+
+
 
 int main() {
 	
-	while (!gameOver) {
+	do{
 		display();
 		currentPlayer();
 		insertPosition(display);
+		gameWinner();
+	} while (gameWinner());
+	display();
+	if (playerTurn == 'X') {
+		cout << "Congratulations! Player with [X] has won the game!" << endl;
+	}
+	else {
+		cout << "Congratulations! Player with [O] has won the game!" << endl;
 	}
 	
 
